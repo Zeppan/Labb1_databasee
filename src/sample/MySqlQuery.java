b3ef7442c4a8a876d99986ec22f2b4b3a18419ad
Hampus <hglantz@kth.se>
2016-12-01 17:17:51 +0100

    Enums

--- src/sample/MySqlQuery.java
+++ src/sample/MySqlQuery.java
@@ -1,11 +1,33 @@
 package sample;
 
+import java.sql.Connection;
+
 /**
  * Created by Glantz on 2016-12-01.
  */
 public class MySqlQuery implements SQLquery{
 
    public void searchForCdByArtis(){
+
+   }
+
+   @Override
+   public void updateItem(Connection con, String query) {
+
+   }
+
+   @Override
+   public void insertItem(Connection con, String query) {
+
+   }
+
+   @Override
+   public void deleteItem(Connection con, String query) {
+
+   }
+
+   @Override
+   public void selectItem(Connection con, String query) {
 
    }
 }
