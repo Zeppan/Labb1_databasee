313e8a404deb037c07abe2b806ce205cf4413762
Hampus <hglantz@kth.se>
2016-12-07 15:33:37 +0100

    lösningar

--- src/Model/content.java
+++ src/Model/content.java
@@ -1,4 +1,5 @@
 package Model;
+
 import java.util.ArrayList;
 
 /**
@@ -11,11 +12,12 @@ public class content {
     private String releaseDate;
     private String type;
     private String addedBy;
-    private ArrayList<Creator> creators;;
+    private ArrayList<Creator> creators;
+    ;
     private ArrayList<genre> genres;
     private ArrayList<review> reviews;
 
-    public content(int contentID, String title, String releaseDate, String type ,int rating){
+    public content(int contentID, String title, String releaseDate, String type, int rating) {
         creators = new ArrayList<>();
         genres = new ArrayList<>();
         reviews = new ArrayList<>();
@@ -24,46 +26,87 @@ public class content {
         this.releaseDate = releaseDate;
         this.type = type;
     }
-    public content (){
+
+    public content() {
         creators = new ArrayList<>();
         genres = new ArrayList<>();
         reviews = new ArrayList<>();
     }
-    public ArrayList<Creator> getCreators(){ return creators;    }
-    public String getTitle(){
+
+    public ArrayList<Creator> getCreators() {
+        return creators;
+    }
+
+    public ArrayList<genre> getGenres() {
+        return genres;
+    }
+
+    public String getTitle() {
         return title;
     }
-    public String getReleaseDate(){
+
+    public String getReleaseDate() {
         return releaseDate;
     }
-    public String getType(){
+
+    public String getType() {
         return type;
     }
-    public String getAddedBy(){return addedBy;}
-    public int getContentID(){return contentID;}
-    public int getRating(){return rating;}
-    public String getReviews(){
+
+    public String getAddedBy() {
+        return addedBy;
+    }
+
+    public int getContentID() {
+        return contentID;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getReviews() {
         String tmp = "";
-        for(review rev : reviews){
+        for (review rev : reviews) {
             tmp += rev.getReview() + ", ";
         }
-        return tmp;}
-
-    public void SetTitle(String Title){this.title=Title;}
-    public void SetReleaseDate(String releaseDate){this.releaseDate=releaseDate;}
-
-    public void SetType(String type){this.type=type; }
-
-    public void SetaddedBy(String addedBy){this.addedBy=addedBy;}
-    public void SetContentID(int contentID){this.contentID=contentID;}
-
-
-    public void addCreator(int creatorID,String name, String nationality, creatorRole role){
-        creators.add(new Creator(creatorID,name,nationality,role));
+        return tmp;
     }
-    public void addGenre(String genr){genres.add(new genre(genr));}
-    public void addReview(String date,String review){reviews.add(new review(date,review));}
-    public void addReview(String rev){
+
+    public void SetTitle(String Title) {
+        this.title = Title;
+    }
+
+    public void SetReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public void SetType(String type) {
+        this.type = type;
+    }
+
+    public void SetaddedBy(String addedBy) {
+        this.addedBy = addedBy;
+    }
+
+    public void SetContentID(int contentID) {
+        this.contentID = contentID;
+    }
+
+
+    public void addCreator(int creatorID, String name, String nationality, String role, String addedBy) {
+        creators.add(new Creator(creatorID, name, nationality, role, addedBy));
+    }
+
+    public void addGenre(String genr) {
+        genres.add(new genre(genr));
+    }
+
+    public void addReview(String date, String review) {
+        reviews.add(new review(date, review));
+    }
+
+    public void addReview(String rev) {
         reviews.add(new review(rev));
     }
 
